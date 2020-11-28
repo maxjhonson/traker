3eb03fd1a751b4cd6d6d1a886566bb00d1c16e9b
Maxwell Jhonson <max.jhonson07@gmail.com>
2020-11-27 22:51:34 -0400

    Init Commit s Please enter the commit message for your changes. Lines starting

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/TrackRoutes');
+const bodyParser = require('body-parser');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+ 
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri='mongodb+srv://admin:8095987482@cluster0.n0sxr.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(mongoUri,{
+    useNewUrlParser:true,
+    useCreateIndex:true
+})
+
+mongoose.connection.on('connected', ()=>{
+    console.log('Connected to mongo instance')
+})
+
+mongoose.connection.on('error', err=>{
+    console.error('Error connection to mongo', err);
+})
+
+
+app.get('/',requireAuth, (req, res)=>{
+    res.send(`Your email: ${req.user.email}`)
+})
+
+app.listen(3000,()=>{
+    console.log("listening on port 3000")
+})
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const User = mongoose.model('User');
+
+const router = express.Router();
+
+router.post('/signup',async (req,res)=>{
+     try{
+        const user = new User({email:req.body.email, password:req.body.password});
+        await user.save();
+
+        const token = jwt.sign({userId:user._id}, 'MY_SECRET_KEY');
+        res.send({token });
+     }catch(err){
+         return res.status(422).send(err.message);
+     }
+})
+ 
+router.post('/signin',async (req,res)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(422).send({error: 'Must provide email and password'})
+    }
+
+    const user = await User.findOne({email});
+    if(!user){
+        return res.status(404).send({error: "Invalid password or email"})
+    }
+
+    try{
+        await user.comparePassword(password);
+        const token= jwt.sign({userId:user._id}, "MY_SECRET_KEY");
+        res.send({token})
+    }
+    catch(err){
+        console.log(err)
+        return res.status(422).send({error:"Invalid password or email"})
+    }
+})
+
+
+
+module.exports = router;
